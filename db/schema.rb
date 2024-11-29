8684e1a90fe6b54fae27e18005e7796605ce08e7
Lubomir-Raragovskiy <114918730+Lubomir-Raragovskiy@users.noreply.github.com>
2024-11-29 02:25:28 -0500

    final commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_28_064245) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_29_070005) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -154,7 +154,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_28_064245) do
     t.string "name"
     t.string "email"
     t.string "phone"
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "address_id"
